f032a7db948b05065ccac449f5f8c59d11eb7e44
virginiafreitas <virginiacdefreitas@gmail.com>
2023-06-30 22:53:19 -0700

    creates function writePassword

--- script.js
+++ script.js
@@ -8,3 +8,9 @@ var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P"
 var numbers = ["1","2","3","4","5","6","7","8","9","0"]
 var specialCharacters = ["!","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","^","_","`","{","|","}","~"]
 
+// Write password to the #password input
+function writePassword() { // creates function "writePassword"
+    var password = generatePassword();   // creates variable "password" with function called generatePasswork()
+    var passwordText = document.querySelector("#password");   // creates variable "passworkText" with function grabbing "id=password" in html
+    passwordText.value = password;   //setting value to password
+  }
